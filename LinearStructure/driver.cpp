3e4fbde8a9e3e97de40c4653e2c562a36252078d
Fengshi <lfs@live.com>
2019-02-01 00:04:16 +0800

    Test theme affection

--- LinearStructure/driver.cpp
+++ LinearStructure/driver.cpp
@@ -5,16 +5,17 @@
 
 using namespace std;
 
-//int main(void)
-//{
-//	vector<int> list1({ 3, 5, 8, 11 });
-//	vector<int> list2({ 2, 6, 8, 9, 11, 15, 20 });
-//	vector<int> res(ordered_union(list1, list2));
-//	for (auto elem : res)
-//	{
-//		cout << elem << ' ';
-//	}
-//	cout << endl;
-//	system("pause");
-//	return 0;
-//}
+int main(void)
+{
+	vector<int> list1({ 3, 5, 8, 11 });
+	vector<int> list2({ 2, 6, 8, 9, 11, 15, 20 });
+	vector<int> res(ordered_union(list1, list2));
+	
+	for (auto elem : res)
+	{
+		cout << elem << ' ';
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
