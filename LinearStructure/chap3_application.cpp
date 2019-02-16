cb13a29fbc146a54570a744919fedaca95dccf19
Fengshi <lfs@live.com>
2019-02-16 16:00:12 +0800

    Add infix evaluation function.

--- LinearStructure/chap3_application.cpp
+++ LinearStructure/chap3_application.cpp
@@ -1,18 +1,25 @@
 //Chap 3 Section 6
+#include <iostream>
 #include <string>
 #include <vector>
 #include <stack>
 #include <map>
+#include <unordered_map>
 #include <set>
+#include <cctype>
+#include <climits>
 
 using std::string;
 using std::vector;
 using std::stack;
 using std::map;
+using std::unordered_map;
 using std::set;
 using std::pair;
 using std::make_pair;
 using std::to_string;
+using std::isdigit;
+using std::ispunct;
 
 //3.6.1, decimal convertor
 string actual_convertor(string str, int radix)
@@ -72,4 +79,122 @@ bool check_parentheses(const string& str)
 	}
 	//If the stack is empty now, then all parentheses match successfully
 	return st.empty();
+}
+
+//3.6.3, evaluate infix expression
+//Support float number elementary arithmatic
+double evaluate_infix_expression(const string& str)
+{
+	if(!check_parentheses(str))
+	{
+		std::cerr << "Invalid infix expression.";
+		return DBL_MIN;
+	}
+	struct expression_term
+	{
+		char operators;
+		double number;
+		bool is_operator;
+	};
+	unordered_map<char, int> support_operators({
+		pair<char, int>('+', 1),
+		pair<char, int>('-', 1),
+		pair<char, int>('*', 2),
+		pair<char, int>('/', 2),
+		});
+
+	//Generate postfix expression
+	vector<expression_term> postfix_expression;
+	stack<char> assist_stack;
+	for (int i = 0; i < str.size(); i++)
+	{
+		//Digit term
+		if (isdigit(str[i]))
+		{
+			int num_len = 1;
+			//Consider number with dot
+			while (i + num_len < str.size() && (isdigit(str[i + num_len]) || str[i + num_len] == '.'))
+				num_len++;
+			//7 is dummy number, change it freely
+			postfix_expression.push_back({ static_cast<char>(7), stoi(str.substr(i, num_len)), false });
+			i += (num_len - 1);
+		}
+
+		//Operator, fxxking ()...
+		else if (support_operators.find(str[i]) != support_operators.end() ||
+			str[i] == '(' || str[i] == ')')
+		{
+			char current_operator = str[i];
+			if (assist_stack.empty() ||
+				support_operators[assist_stack.top()] < support_operators[current_operator] ||
+				current_operator == '(')
+			{
+				assist_stack.push(current_operator);
+			}
+			else
+			{
+				while (!assist_stack.empty() &&
+					(support_operators[assist_stack.top()] >= support_operators[current_operator] ||
+						current_operator == ')'))
+				{
+					if (assist_stack.top() == '(' && current_operator == ')')
+					{
+						assist_stack.pop();
+						break;
+					}
+					postfix_expression.push_back({ assist_stack.top(), -1, true });
+					assist_stack.pop();
+				}
+				if (current_operator != ')')
+					assist_stack.push(current_operator);
+			}
+		}
+		//Other characters
+		else
+		{
+			std::cerr << "Invalid infix expression.";
+			return DBL_MIN;
+		}
+	}
+	//Let out the rest operators
+	while (!assist_stack.empty())
+	{
+		postfix_expression.push_back({ assist_stack.top(), -1, true });
+		assist_stack.pop();
+	}
+
+	//Use postfix expression to evaluate value
+	stack<double> st;
+	for (const auto& elem : postfix_expression)
+	{
+		if (elem.is_operator)
+		{
+			//Note that operand2 is at front
+			double operand2 = st.top();
+			st.pop();
+			double operand1 = st.top();
+			st.pop();
+			char current_operator = elem.operators;
+			switch (current_operator)
+			{
+			case '+':
+				st.push(operand1 + operand2);
+				break;
+			case '-':
+				st.push(operand1 - operand2);
+				break;
+			case '*':
+				st.push(operand1 * operand2);
+				break;
+			case '/':
+				st.push(operand1 / operand2);
+				break;
+			}
+		}
+		else
+		{
+			st.push(elem.number);
+		}
+	}
+	return st.top();
 }
--- LinearStructure/driver.cpp
+++ LinearStructure/driver.cpp
@@ -1,25 +1,129 @@
 #include <iostream>
-#include "linear_seq.h"
-#include "linear_linked.h"
-#include "prac_stack.h"
-#include "prac_queue.h"
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <set>
+#include <cctype>
 
 using namespace std;
 
-bool check_parentheses(const string& str);
+double evaluate_infix_expression(const string& str);
 
 int main(void)
 {
-	string input("(()]");
-	if (check_parentheses(input))
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	string input;
+	while (getline(cin, input) && input != "0")
 	{
-		cout << "Parentheses match successfully.\n";
+		cout.precision(2);
+		cout.setf(ios_base::fixed);
+		cout << evaluate_infix_expression(input) << '\n';
 	}
-	else
+	//system("pause");
+	return 0;
+}
+
+double evaluate_infix_expression(const string& str)
+{
+	struct expression_term
 	{
-		cout << "Match failed.\n";
+		char operators;
+		int number;
+		bool is_operator;
+	};
+	unordered_map<char, int> support_operators({
+		pair<char, int>('+', 1),
+		pair<char, int>('-', 1),
+		pair<char, int>('*', 2),
+		pair<char, int>('/', 2),
+		});
+	//Generate postfix expression
+	vector<expression_term> postfix_expression;
+	stack<char> assist_stack;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (isdigit(str[i]))
+		{
+			int num_len = 1;
+			while (i + num_len < str.size() && isdigit(str[i + num_len]))
+				num_len++;
+			postfix_expression.push_back({ '.', stoi(str.substr(i, num_len)), false });
+			i += (num_len - 1);
+		}
+		else if (support_operators.find(str[i]) != support_operators.end() || 
+			str[i] == '(' || str[i] == ')')
+		{
+			char current_operator = str[i];
+			if (assist_stack.empty() ||
+				support_operators[assist_stack.top()] < support_operators[current_operator] || 
+				current_operator == '(')
+			{
+				assist_stack.push(current_operator);
+			}
+			else
+			{
+				while (!assist_stack.empty() &&
+					(support_operators[assist_stack.top()] >= support_operators[current_operator] ||
+						current_operator == ')'))
+				{
+					if (assist_stack.top() == '(' && current_operator == ')')
+					{
+						assist_stack.pop();
+						break;
+					}
+					postfix_expression.push_back({ assist_stack.top(), -1, true });
+					assist_stack.pop();
+				}
+				if (current_operator != ')')
+					assist_stack.push(current_operator);
+			}
+			
+		}
+		else
+		{
+			continue;
+		}
+	}
+	while (!assist_stack.empty())
+	{
+		postfix_expression.push_back({ assist_stack.top(), -1, true });
+		assist_stack.pop();
 	}
 
-	system("pause");
-	return 0;
+	//Use postfix expression to evaluate value
+	stack<double> st;
+	for (const auto& elem : postfix_expression)
+	{
+		if (elem.is_operator)
+		{
+			double operand2 = st.top();
+			st.pop();
+			double operand1 = st.top();
+			st.pop();
+			char current_operator = elem.operators;
+			switch (current_operator)
+			{
+			case '+':
+				st.push(operand1 + operand2);
+				break;
+			case '-':
+				st.push(operand1 - operand2);
+				break;
+			case '*':
+				st.push(operand1 * operand2);
+				break;
+			case '/':
+				st.push(operand1 / operand2);
+				break;
+			}
+		}
+		else
+		{
+			st.push(elem.number);
+		}
+	}
+	return st.top();
 }
